68d449dffc690b45092db63acbd360d6e219debf
Pervez <pervezmuhamed@gmail.com>
2022-11-09 15:30:51 +0530

    Added the files

---
+++ App.js
@@ -0,0 +1,88 @@
+// function ask (question,yes,no) {
+//     let age = prompt (question, 18);
+//     if(age>=18) {
+//         yes()
+//     } else {
+//         no()
+//     }
+// }
+
+// function Showok () {
+//     alert("YOu are Eligible to Vote");
+// }
+// function Showcancel () {
+//     alert("You are not Eligible to Vote");
+// }
+
+// ask("What your Age ?",Showok,Showcancel);
+
+
+// let Salaries ={john:100,Anne:160,Pete:130,};
+
+// let sum =0;
+// for(let key in Salaries) {
+//     sum += Salaries[key]
+// };
+// console.log(sum);
+
+// let Calculator ={
+
+// };
+
+
+// Calculator.read()
+// alert(Calculator.sum());
+// alert(Calculator.mul());
+
+
+// function Calculator () {
+//     this.read = function() {
+//         this.a= + prompt("Value of a is?",0);
+//         this.b =+ prompt("Value of b is?",0);
+//     };
+//     this.sum=function() {
+//         return this.a + this.b;
+//     }
+//     this.mul=function() {
+//         return this.a * this.b;
+//     }
+        
+// }
+// let calculator =new Calculator();
+// calculator.read();
+
+// alert("sum=" + calculator.sum());
+// alert("sum=" + calculator.mul());
+
+// function Accumulator(startingvalue) {
+//     this.value =startingvalue;
+
+
+//     this.read=function() {
+//         this.value += +prompt("value is",0);
+//     }
+// }
+// let accumulator =new Accumulator(5);
+
+// accumulator.read();
+// accumulator.read();
+
+// alert(accumulator.value);
+
+// let Styles =["jazz","Music"];
+
+// Styles.push("Rool-out-roll");
+// let mat = Math.trunc(Styles.length-1/2);
+// console.log(mat);
+
+// let recipes=new Map([['carrot', 40],
+// ['tomato',50],['cucumber',70],['ladyfinger',30]]  );
+
+// for(vegetables of recipes.keys()){
+//     alert(vegetables);
+// }
+// for(price of recipes.values()){
+//     alert(price);
+// }  
+// for(entry of recipes){
+//     alert(entry
